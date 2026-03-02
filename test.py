dec0eb168d5d8fb17aead7a2c31652c32d705fd9
Aradea Satria <aradeasatria18@gmail.com>
2026-03-02 10:51:36 +0700

    update code origin feature-login

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print ("Hello World")
+username_benar = "admin"
+password_benar = "123"
+
+# Input dari user
+username = input("Masukkan username: ")
+password = input("Masukkan password: ")
+
+# Proses pengecekan
+if username == username_benar and password == password_benar:
+    print("Login berhasil!")
+else:
+    print("Username atau password salah!")
